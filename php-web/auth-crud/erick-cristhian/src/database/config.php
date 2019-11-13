505481fef8f64b7e00bff20ba3fedda879b720dc
ErickCristhian <erickcristhian123@gmail.com>
2019-11-13 20:30:50 -0300

    Desafio Auth-crud

---
+++ php-web/auth-crud/erick-cristhian/src/database/config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+const DB = 'mysql';
+const DBHOST = 'localhost';
+const DBNAME = 'computer';
+const DBUSER = 'root';
+const DBPWD = '';
+*/
+
+const DB = 'mysql';
+const DBHOST = 'mysql';
+const DBNAME = 'computer';
+const DBUSER = 'root';
+const DBPWD = 'abc123';
